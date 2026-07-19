9d56fbdf027b5ce679ceb6386635330261154ba1
Aaron Schlesinger <70865+arschles@users.noreply.github.com>
2026-07-19 14:23:08 -0700

    removing unnecessary import

--- cli/src/builtins/mod.rs
+++ cli/src/builtins/mod.rs
@@ -10,7 +10,6 @@ mod simpleqa_verified;
 
 pub use custom_nocode::CustomNoCodeBuiltin;
 pub use custom_nocode::metrics::{
-    OutputMetric as CustomNoCodeOutputMetric,
     compute_output_metrics as compute_custom_nocode_output_metrics,
     output_metrics_requested as custom_nocode_output_metrics_requested,
 };
